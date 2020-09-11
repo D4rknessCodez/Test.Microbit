7d7bbdca41e56d504146e835b0b65da8a295edfd
Amine <41171010+D4rknessCodez@users.noreply.github.com>
2020-09-11 15:09:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -52,7 +52,48 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 input.onButtonPressed(Button.B, function () {
-	
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
